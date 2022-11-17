67ad94a0f69789152e376ffa2dc76340fd14c775
akshatcoder-hash <akshat@shastraos.co>
2022-11-17 22:38:00 +0530

    :construction_worker: Calculator.py

--- programs_py/calculator.py
+++ programs_py/calculator.py
@@ -4,3 +4,26 @@
 from seahorse.prelude import *
 
 declare_id('Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS')
+
+class Calculoator(Account):
+	owner: Pubkey
+	display: i64
+
+@instruction
+def init_calculator(owner: Signer, calculator: Empty[Calculator]):
+	  calculator = calculator.init(
+   	 payer = owner,
+   	 seeds = ['Calculator', owner]
+ 	 )
+ 	 calculator.owner = owner.key()
+
+@instruction
+def reset_calculator(owner: Signer, calculator: Calculator):
+  print(owner.key(), 'is resetting', calculator.key())
+
+
+  # Verify owner
+  assert owner.key() == calculator.owner, 'This is not your calculator!'
+
+
+  calculator.display = 0
